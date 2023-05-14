9c22ca5b80fe974da1546c3b45cfe0c29b7a2b8f
badhon09 <badhonghosh09@gmail.com>
2023-05-14 12:15:53 +0600

    add to cart

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -8,6 +8,7 @@ export const Navbar = () => {
     useEffect(() => {
       const storedCartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
       setCartItems(storedCartItems);
+      console.log(cartItems);
     }, []);
 
 
@@ -19,18 +20,18 @@ export const Navbar = () => {
                 <div className="product">
                     <div className="product-cart-details">
                         <h4 className="product-title">
-                            <a href="product.html">Beige knitted elastic runner shoes</a>
+                            <a href="product.html">{items.title}</a>
                         </h4>
     
                         <span className="cart-product-info">
                             <span className="cart-product-qty">1</span>
-                            x $84.00
+                            x ${items.price}
                         </span>
                     </div>
     
                     <figure className="product-image-container">
                         <a href="product.html" className="product-image">
-                            <img src="assets/images/products/cart/product-1.jpg" alt="product"/>
+                            <img src={items.image} alt="product"/>
                         </a>
                     </figure>
                     <a href="#" className="btn-remove" title="Remove Product"><i className="icon-close"></i></a>
--- src/components/parts/Product.jsx
+++ src/components/parts/Product.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 export const Product = (item) => {
+
+    const addToCart = document.getElementById('cart-btn');
+
+    const addCart=(e)=>{
+       
+        const productId = e.proid;
+        const title = e.title;
+        const price = e.price;
+        const qty = 1
+        const image = e.image;
+        const product = { id: productId, title: title, price: price,image:image , qty:qty }; // Replace with actual product details
+        let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+        cartItems.push(product);
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    }
+
+   
   return (
     <>
-    <div className="col-6 col-md-4 col-lg-3" key={item.id}>
+    <h1>{item.item.id}</h1>
+    <div className="col-6 col-md-4 col-lg-3" key={item.item.id}>
                          <div className="product product-2">
                              <figure className="product-media">
                                  <span className="product-label label-circle label-sale">Sale</span>
                                  <a href="product.html">
-                                     <img src={item.image} alt="Product image" width="200px" className="product-image"/>
+                                     <img src={item.item.image} alt="Product image" width="200px" className="product-image"/>
                                  </a>
 
                                  <div className="product-action-vertical">
@@ -17,7 +35,7 @@ export const Product = (item) => {
                                  </div>
 
                                  <div className="product-action">
-                                     <a href="#" className="btn-product btn-cart" id={item.id} title={item.title} image={item.image} price={item.price} onClick={handleAddToCart(item)}><span>add to cart</span></a>
+                                     <button id="cart-btn" className="btn-product btn-cart" proid={item.item.id} title={item.item.title} image={item.item.image} price={item.item.price} onClick={(e) => addCart(item.item)}><span>add to cart</span></button>
                                      <a href="popup/quickView.html" className="btn-product btn-quickview" title="Quick view"><span>quick view</span></a>
                                  </div>
                              </figure>
@@ -26,16 +44,16 @@ export const Product = (item) => {
                                  <div className="product-cat">
                                      <a href="#">Headphones</a>
                                  </div>
-                                 <h3 className="product-title"><Link to={'/product-details/'+item.id} >{item.title}</Link></h3>
+                                 <h3 className="product-title"><Link to={'/product-details/'+item.item.id} >{item.item.title}</Link></h3>
                                  <div className="product-price">
-                                     <span className="new-price">${item.price}</span>
+                                     <span className="new-price">${item.item.price}</span>
                                      <span className="old-price">Was $349.99</span>
                                  </div>
                                  <div className="ratings-container">
                                      <div className="ratings">
                                          <div className="ratings-val" ></div>
                                      </div>
-                                     <span className="ratings-text">( {item.rating[0]} )</span>
+                                     <span className="ratings-text">( {item.item.rating[0]} )</span>
                                  </div>
 
                                  <div className="product-nav product-nav-dots">
