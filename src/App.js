fbb4f6e2311e3187a61cc3ec80fceb5805a961e4
badhon09 <badhonghosh09@gmail.com>
2023-05-15 18:00:34 +0600

    login added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter,Routes,Route, Navigate } from "react-router-dom";
 import {Home} from './components/pages/Home';
 import { ProductDetails } from "./components/pages/ProductDetails";
 import { Login } from "./components/pages/Login";
+import { AuthContext } from "./context/AuthContext";
+import { useContext } from "react";
+import { Checkout } from "./components/pages/Checkout";
 
 function App() {
+
+
+  const ProtectedRoute = ({ children }) => {
+    const { user } = useContext(AuthContext);
+
+    if (!user) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
+
+
   return (
     <>
     <BrowserRouter>
@@ -11,6 +28,10 @@ function App() {
       <Route path="/" element={<Home/>} />
       <Route path="/product-details/:id" element={<ProductDetails/>} />
       <Route path="/login" element={<Login/>} />
+      
+      <Route path="/checkout" element={<ProtectedRoute><Checkout/>  </ProtectedRoute>} />
+    
+     
     </Routes>
     </BrowserRouter>
     </>
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,10 +1,127 @@
-import React from 'react'
+import React, { useContext,useState } from 'react'
 import { Layout } from '../layouts/Layout'
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { AuthContext } from '../../context/AuthContext';
 
 export const Login = () => {
+
+  const [credentials , setCredentials] = useState({
+    // email:undefined,
+    // password:undefined,
+  });
+
+  const {loading,error,dispatch} = useContext(AuthContext )
+  const navigate = useNavigate();
+
+  const handleChange = (e) =>{
+    setCredentials((prev)=>({
+      ...prev,
+      [e.target.name]: e.target.value 
+    }))
+  }
+
+  const handleSubmit = async (e) =>{  
+    
+    e.preventDefault();
+    dispatch({type:"LOGIN_START"});
+    console.log(credentials);
+    try{
+
+      const res = await axios.post('http://127.0.0.1:8001/api/auth/login',credentials)
+      
+     // console.log(res.data);
+     
+      if(res.data){
+
+        dispatch({type:"LOGIN_SUCCESS", payload:res.data})
+        navigate("/");
+    
+      }else{
+
+        dispatch({
+          type:"LOGIN_FAILURE",
+          payload : {message: "you are not allowed"}
+        })
+      }
+
+    }catch(err){
+      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
+    }
+
+
+  }
+
+
+
   return (
     <Layout>
-        
+        <div className="container">
+
+          <section className="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+                  <div className="d-flex justify-content-center py-4">
+                    <a href="index.html" className="logo d-flex align-items-center w-auto">
+                      <img src="assets/img/logo.png" alt=""/>
+                      <span className="d-none d-lg-block">Hotel Booking</span>
+                    </a>
+                  </div>
+
+                  <div className="card mb-3">
+
+                    <div className="card-body">
+
+                      <div className="pt-4 pb-2">
+                        <h5 className="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                        <p className="text-center small">Enter your email & password to login</p>
+                      </div>
+
+                      <form className="row g-3 needs-validation" onSubmit={handleSubmit}>
+
+                        <div className="col-12">
+                          <label for="yourUsername" className="form-label">Email</label>
+                          <div className="input-group has-validation">
+                            {/* <span className="input-group-text" id="inputGroupPrepend">@</span> */}
+                            <input type="email" name="email" onChange={handleChange} className="form-control"  required/>
+                            <div className="invalid-feedback">Please enter your email.</div>
+                          </div>
+                        </div>
+
+                        <div className="col-12">
+                          <label for="yourPassword" className="form-label">Password</label>
+                          <input type="password" name="password" onChange={handleChange}  className="form-control" id="yourPassword" required/>
+                          <div className="invalid-feedback">Please enter your password!</div>
+                        </div>
+
+                        <div className="col-12">
+                          <div className="form-check">
+                            <input className="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe"/>
+                            <label className="form-check-label" for="rememberMe">Remember me</label>
+                          </div>
+                        </div>
+                        <div className="col-12">
+                          <button className="btn btn-primary w-100" type="submit">Login</button>
+                        </div>
+                        <div className="col-12">
+                          <p className="small mb-0">Don't have account? <a href="pages-register.html">Create an account</a></p>
+                        </div>
+                      </form>
+
+                    </div>
+                  </div>
+
+                
+
+                </div>
+              </div>
+            </div>
+
+          </section>
+
+          </div>
     </Layout>
   )
 }
